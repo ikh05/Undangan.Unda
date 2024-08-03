3cc851f0ccade4a607b8ad72a7bd12fe00fc91ff
ikh05 <mi3061923002@gmail.com>
2024-08-03 22:33:55 +0800

    tes

--- app/const.php
+++ app/const.php
@@ -1,7 +1,11 @@
 <?php 
-
-	define('BASE_URL', 'http://localhost/Undangan.ku/');
-	var_dump($_SERVER);
-	die;
+	if($_SERVER['SERVER_NAME'] === 'localhost'){
+		define('BASE_URL', 'http://localhost/Undangan.ku/');
+	}elseif($_SERVER['SERVER_PROTOCOL'] === 'HTTP/1.1'){
+		define('BASE_URL', 'http://Undangan-Unda.ich.free.nf/');
+	}else{
+		define('BASE_URL', 'https://Undangan-Unda.ich.free.nf/');
+	}
+	var_dump(BASE_URL);
 
  ?> 
