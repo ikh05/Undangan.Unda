2c851f0515506db06499cb7813f3e3c88ac8a8bd
ikh05 <mi3061923002@gmail.com>
2024-08-04 07:31:55 +0800

    tujuan undangan

--- app/views/Undangan/Zulmi_Latifah.php
+++ app/views/Undangan/Zulmi_Latifah.php
@@ -7,7 +7,7 @@
 		--text-coklat: #3B3633;
 	}
 
-	.family-pinyon{
+	.family-pinyon{ 
 		font-family: 'Pinyon Script', sans-serif;
 	}
 	.family-roboto{
@@ -16,7 +16,7 @@
 	.family-spectral{
 		font-family: 'Spectral', sans-serif;
 	}
-	.bold{
+	.text-bold{
 		font-weight: bold;
 	}
 
@@ -52,11 +52,11 @@
 		<img class="position-absolute top-0 start-0 translate-middle" src="<?= BASE_URL ?>assets/img/Zulmi_Latifah/kembang 1.png" alt="kembang 1" style="max-width: 50vw;">						
 		<img class="position-absolute top-100 start-100 translate-middle" src="<?= BASE_URL ?>assets/img/Zulmi_Latifah/kembang 2.png" alt="kembang 2" style="max-width: 50vw;">						
 	</div>
-	
+
 	<div class="container position-relative" style="height: 100%; width: 100%;">
 		<div class="position-relative family-pinyon text-end pt-3" style="z-index: 2;">
-			<h1 class="bold me-5 display-3">Undangan</h1>
-			<h1 class="bold display-3">Pernikahan</h1>
+			<h1 class="text-bold me-5 display-3">Undangan</h1>
+			<h1 class="text-bold display-3">Pernikahan</h1>
 		</div>
 		<div class=" gambar-mempelai position-absolute start-50 translate-middle-x" style="height: 50vh;">
 			<img class="position-relative start-50 top-0 translate-middle-x" src="<?= BASE_URL ?>assets/img/Zulmi_Latifah/Cover.png" alt="gambar-mempelai" style="max-height: 100%;">
@@ -67,11 +67,11 @@
 			<div class="col">&</div>
 			<div class="col-5">Zulmi</div>
 		</div> -->
-		<div class="container position-absolute bottom-0 start-50 translate-middle-x mb-5 text-center">
+		<div class="container position-absolute bottom-0 start-50 translate-middle-x mb-5 text-center pb-md-0 pb-5">
 			<h3 class="">Kepada Yth.</h3>
 			<h3 class=""><?= $data['to'] ?></h3>
 			<p class="">Mohon maaf, apabila ada kesalahan dalam penulisan nama dan gelar</p>
-			<a href="#undangan-main" name="buka-undangan" class="btn btn-info">Buka Undangan</a>
+			<a href="#undangan-main" name="buka-undangan" class="btn btn-info text-bold bold family-roboto">Buka Undangan</a>
 		</div>
 	</div>
 </section>
